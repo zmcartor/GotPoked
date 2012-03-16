6bc6ac375b43e65d3fe3d4c17895b98e4c6451e3
Zach <zach@hackazach.net>
2012-03-16 09:06:00 -0400

    first try at server

--- poked.rb
+++ poked.rb
@@ -3,38 +3,28 @@ require 'json'
 require 'yaml'
 
 #Sinatra Classic app
+$config_file = ARGV[0] || 'repo_map.yml'
+$configuration = YAML::load_file($config_file)
 
-SECRET = "supercool"
-
-configure do
-	Log = Logger.new("debug.log")
-	Log.level = Logger::INFO
-end
-
-before do
-	puts "GOT THIS"
-	p params
-end
-
-config_file = ARGV[0] || 'repo_map.yml'
-configuration = YAML::load_file(config_file)
-
-def our_local_path(remote_repo)
-	configuration['repos'][remote_repo]
-end
-
-post '/:secret' do
-	halt unless params[:secret].eq? SECRET
-	json_push = JSON.parse(params[:payload])
-	local_repo = our_local_path json_push['repository']['name']
-	
+post '/poked' do
+	json_push = JSON.parse params[:payload]
+	local_repo = $configuration[json_push['repository']['name']]
 	#run shell command on server..
+	puts "Updating #{local_repo}"
 	`cd #{local_repo} && git pull origin master`
 	"pokeD OK"
 end
 
+before '/:r' do
+		puts "GOT THIS"
+	p params
+end
+
 get '/:r' do
-	local_repo = our_local_path params[:r]
-	Log.info "doing local repo: #{local_repo} "
-	`cd #{local_repo} && git pull origin master`
+	#sometimes wants to grab favicon. stupid..
+	pass if params[:r] == 'favicon.ico'
+	local_repo = $configuration[params[:r]]
+	p local_repo
+	out = `cd #{local_repo} && git pull origin master`
+	p out
 end
